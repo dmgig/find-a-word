9200b32c7965bfa6a88ee381bdfa09efc78b0221
Dave M. Giglio <dave.m.giglio@gmail.com>
2016-07-22 00:49:08 -0400

    add selectB method, checkWord method

--- game.js
+++ game.js
@@ -3,6 +3,49 @@
   
   console.log(data);
   
+  // http://stackoverflow.com/questions/13491676/get-all-pixel-coordinates-between-2-points
+  var lineMath = function(A, B){
+    
+    A = [Number(A[0]), Number(A[1])];
+    B = [Number(B[0]), Number(B[1])];
+    
+    console.log(A);
+    console.log(B);
+    
+    function slope(a, b) {
+      if (a[0] == b[0]) {
+        return null;
+      }
+      return (b[1] - a[1]) / (b[0] - a[0]);
+    }
+
+    function intercept(point, slope) {
+      
+      console.log(point)
+      console.log(slope)
+      
+      if (slope === null) {
+        // vertical line
+        return point[0];
+      }
+      return point[1] - slope * point[0];
+    }
+
+    var m = slope(A, B);
+    var b = intercept(A, m);
+
+    console.log(m);
+    console.log(b);
+
+    var coordinates = [];
+    for (var x = A[0]; x <= B[0]; x++) {
+      var y = m * x + b;
+      coordinates.push([x, y]);
+    }
+    
+    return coordinates;
+  }
+  
   var wordfinder = {
     init: function(){
       console.log('init');
@@ -49,8 +92,8 @@
     
     gameboard: {
       state: 'startup',
-      selectedA: [],
-      selectedB: [],
+      selected_a: [],
+      selected_b: [],
       create: function(){
         
         this.state = 'waiting'
@@ -84,12 +127,18 @@
         console.log($(this));
         
         var state = wordfinder.gameboard.state;
+        console.log(state);
         
         if(state == 'waiting'){
           $(this).addClass('selectA');
           wordfinder.gameboard.state = 'selectA';
-          wordfinder.gameboard.selectedA = [$(this).data('x'), $(this).data('y')]
+          wordfinder.gameboard.selected_a = [$(this).data('x'), $(this).data('y')]
         }else if(state == 'selectA'){
+          if($(this).hasClass('outofline')) return;
+          $(this).addClass('selectB');
+          wordfinder.gameboard.state = 'selectB';
+          wordfinder.gameboard.selected_b = [$(this).data('x'), $(this).data('y')]          
+          wordfinder.gameboard.checkWord();
           
         }else if(state == 'selectB'){
           
@@ -101,32 +150,53 @@
         var state = wordfinder.gameboard.state;
         if(state != 'selectA') return;
         
-        var selectedA = wordfinder.gameboard.selectedA;
+        var selected_a = wordfinder.gameboard.selected_a;
         var thissquare = [$(this).data('x'), $(this).data('y')];
         
-        console.log(selectedA);
+        console.log(selected_a);
         console.log(thissquare);
         
-        if(selectedA[0] == thissquare[0] ||
-           selectedA[1] == thissquare[1] ||
-           selectedA[0] - thissquare[0] == selectedA[1] - thissquare[1]){
+        if(selected_a[0] == thissquare[0] ||
+           selected_a[1] == thissquare[1] ||
+           selected_a[0] - thissquare[0] == selected_a[1] - thissquare[1]){
             $(this).addClass('inline');
         }else{
             $(this).addClass('outofline');
         }
         
       },
-      
+
       clearInLine: function(){
         $(this).removeClass('inline');
         $(this).removeClass('outofline')
       },
       
-      checkWord: function(selectedA, selectedB){
+      checkWord: function(){
+        
+        var selected_a = wordfinder.gameboard.selected_a;
+        var selected_b = wordfinder.gameboard.selected_b;
+        
+        var selected_word = this.getWord(selected_a, selected_b);
+        
+        console.log(selected_word);
         
       },
-      
-      selectB: function(gamesquare){
+
+      getWord: function(selected_a, selected_b){
+        
+        var wordarr = []
+        
+        var coordinates = lineMath(selected_a, selected_b)
+
+        for(var i in coordinates){
+          var c = coordinates[i];
+          var letter = $('tr:eq('+c[0]+') td:eq('+c[1]+')').text();
+          wordarr.push(letter);
+        }
+        var word = wordarr.join('');
+        console.log('word is: '+word);
+        
+        return word;
         
       }
       
