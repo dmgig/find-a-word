4c5a0d4db6e25b840237c007c2e38419125e4809
Dave M. Giglio <dave.m.giglio@gmail.com>
2016-07-22 23:23:36 -0400

    add lineMath tests

--- tests.js
+++ tests.js
@@ -1,1 +1,51 @@
+/** out */
 
+QUnit.test( "lineMath 0 degree", function( assert ) {
+  var A = [0,2], B = [0,0];
+  var coords = lineMath(A, B);
+  assert.deepEqual( coords, [A, [0,1], B] );
+});
+
+QUnit.test( "lineMath 45 degree", function( assert ) {
+  var A = [0,2], B = [2,0];
+  var coords = lineMath(A, B);
+  assert.deepEqual( coords, [A, [1,1], B] );
+});
+
+QUnit.test( "lineMath 90 degree", function( assert ) {
+  var A = [0,2], B = [2,2];
+  var coords = lineMath(A, B);
+  assert.deepEqual( coords, [A, [1,2], B] );
+});
+
+QUnit.test( "lineMath 135 degree", function( assert ) {
+  var A = [0,2], B = [2,4];
+  var coords = lineMath(A, B);
+  assert.deepEqual( coords, [A, [1,3], B] );
+});
+
+/** in **/
+
+QUnit.test( "lineMath 180 degree", function( assert ) {
+  var A = [0,0], B = [0,2];
+  var coords = lineMath(A, B);
+  assert.deepEqual( coords, [A, [0,1], B] );
+});
+
+QUnit.test( "lineMath 225 degree", function( assert ) {
+  var A = [2,0], B = [0,2];
+  var coords = lineMath(A, B);
+  assert.deepEqual( coords, [A, [1,1], B] );
+});
+
+QUnit.test( "lineMath 270 degree", function( assert ) {
+  var A = [2,2], B = [0,2];
+  var coords = lineMath(A, B);
+  assert.deepEqual( coords, [A, [1,2], B] );
+});
+
+QUnit.test( "lineMath 315 degree", function( assert ) {
+  var A = [2,4], B = [0,2];
+  var coords = lineMath(A, B);
+  assert.deepEqual( coords, [A, [1,3], B] );
+});
